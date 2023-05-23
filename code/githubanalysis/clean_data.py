1ad97afa617cd52b34724058c8cf7ed3c5c6b954
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 18:37:23 +0100

    add file docstring and comments for what data cleaning steps are to be added to script

--- code/githubanalysis/clean_data.py
+++ code/githubanalysis/clean_data.py
@@ -1,16 +1,16 @@
+"""Data cleaning workflow for github repo analysis."""
+
 import sys
-
-#import bits
-
 import repo_name_clean as name_clean
 
-
-# other stuff
-
-# loop over github urls to do cleaning
+# data cleaning stuff:
+    # handle github url input (read in from file? / github API call?)
+    # loop over github urls using repo_name_clean() to do cleaning
+    # save clean data out
+    # pass clean data to analysis script
 
 def main():
-    repo_name = sys.argv[1] # TODO: remove this once debugged and running at commandline
+    repo_name = sys.argv[1] # TODO: remove this once using read-in data instead of commandline
     name_clean.repo_name_clean(repo_name)
 
 # this bit
