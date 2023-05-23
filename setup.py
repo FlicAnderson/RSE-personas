52d55da59dc4b682ddd37e07a3fa6c7f33fb5f14
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 19:41:00 +0100

    move setup.py to sit outside code folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Code for analysing research software github repositories',
     author='Flic Anderson',
     packages=find_packages(),
-    package_dir={'githubanalysis': './githubanalysis'}
+    package_dir={'githubanalysis': 'code/githubanalysis'}
 )
 
 # via https://stackoverflow.com/a/50194143
