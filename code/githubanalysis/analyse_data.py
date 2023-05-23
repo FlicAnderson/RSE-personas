7cb685e23fdcc6bebc843c0df07a3f05a713ab8b
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 18:35:54 +0100

    add placeholder scripts for data analysis and visualisation

---
+++ code/githubanalysis/analyse_data.py
@@ -0,0 +1,16 @@
+"""Data analysis workflow for github repo analysis."""
+
+# import modules
+
+# data cleaning stuff:
+    # accept / load clean github data for analysis
+    # do analysis :D
+    # save analysis results out
+    # pass analysis results for optional visualisation
+
+# def main():
+#     # things
+#
+# # this bit
+# if __name__ == "__main__":
+#     main()
