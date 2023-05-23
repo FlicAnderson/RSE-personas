7e8552805fc6154dacfbaeb806ad83971dd4ca49
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 18:40:02 +0100

    remove unused sys import; remove todos; add descriptive section comments

--- code/githubanalysis/repo_name_clean.py
+++ code/githubanalysis/repo_name_clean.py
@@ -1,7 +1,5 @@
 """Cleaning github repository names and urls of github repos."""
 
-import sys
-
 def repo_name_clean(repo_url):
     """
     Remove trailing slashes and github url root from string repo names.
@@ -22,13 +20,9 @@ def repo_name_clean(repo_url):
     # to run from terminal @ coding-smart/code:
     $ python githubanalysis/clean_data.py https://github.com/riboviz/riboviz/
     riboviz/riboviz
-
-
     """
 
-# TODO: is it worth setting a default value for testing? e.g. repo_url = 'https://github.com/testperson/testrepo'
-# TODO: how can I handle commandline input? e.g. sys.argv[1]/[1:]
-
+    # handle bad input types
     if isinstance(repo_url, list):
         raise TypeError('Ensure input url is a string. If list of repos, use list comprension e.g. `[repo_name_clean(x) for x in repo_list]`.')
 
@@ -42,6 +36,7 @@ def repo_name_clean(repo_url):
         if ";" in repo_url:
             raise ValueError('Input contains semicolons - ensure input is string of ONE repo only.')
 
+    # do repo url cleaning
         try:
             repo_url = (repo_url.split("https://github.com/"))[1]
             repo_name = (repo_url.rstrip("/"))
@@ -50,6 +45,7 @@ def repo_name_clean(repo_url):
             print(
                 f"Could not clean repo_url into username/repo_name format. Confirm input is correct (and if url is given that it starts 'https://github.com/').")
 
+    # finish
         print(repo_name)
         return repo_name
 
