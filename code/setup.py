48bfb1bc3dfdb451fd55788d550e5f66f1ef0cac
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 16:13:54 +0100

    create setup.py script to create package using setuptools to allow importing code as odules between different folder levels etc

---
+++ code/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='project-code', version='1.0', packages=find_packages())
+
+
+# todo install setuptools (DONE)
+# todo update condaenv.yml
+# todo follow https://stackoverflow.com/a/50194143
