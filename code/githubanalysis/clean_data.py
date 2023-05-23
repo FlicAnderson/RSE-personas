a4ecbc9cdb108819ea819ca79842dd53be566f16
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-23 16:11:34 +0100

    rename github code folder to avoid punctuation

---
+++ code/githubanalysis/clean_data.py
@@ -0,0 +1,19 @@
+import sys
+
+#import bits
+
+import repo_name_clean as name_clean
+
+
+# other stuff
+
+# loop over github urls to do cleaning
+
+def main():
+    repo_name = sys.argv[1] # TODO: remove this once debugged and running at commandline
+    name_clean.repo_name_clean(repo_name)
+
+# this bit
+if __name__ == "__main__":
+    main()
+
