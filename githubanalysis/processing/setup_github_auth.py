613f0b984fda82fd551d85ba9a27051407564d5e
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-24 17:25:57 +0100

    add default param ../config.cfg and new param per_page=100

--- githubanalysis/processing/setup_github_auth.py
+++ githubanalysis/processing/setup_github_auth.py
@@ -5,11 +5,13 @@ from pathlib import Path
 from github import Github
 
 
-def setup_github_auth(configfilepath):
+def setup_github_auth(configfilepath = '../config', per_page=100):
     """
     Authenticates with Github API using user-generated config.cfg file contents; sets per_page to 100 items.
-    :param configfilepath: file path of config.cfg file
+    :param configfilepath: file path of config.cfg file. Default='../config'.
     :type str:
+    :param per_page: number of items per page. Default=100, overwrites Github default 30.
+    :type int:
     :returns: `ghlink`
     :type github.MainClass.Github:
 
@@ -18,7 +20,11 @@ def setup_github_auth(configfilepath):
     TODO.
     """
 
-    # check input (using separate variable to avoid overwriting it as pathlib Path type)
+    # ensure arguments are valid
+    if per_page > 100:
+        raise ValueError('GH API maximum per_page value is 100.')
+
+    # check config filepath input (using separate variable to avoid overwriting it as pathlib Path type)
     pathchecker = Path(configfilepath)
     if pathchecker.exists() == False:
         raise OSError('Config file does not exist at that path.')
@@ -32,7 +38,7 @@ def setup_github_auth(configfilepath):
     try:
         ghlink = Github(
             access_token,
-            per_page=100
+            per_page=per_page
         )  # type(ghlink) is github.MainClass.Github; ghlink.per_page should return 100
 
         print('GH link opened')
