593fcaaaec4fd5946a073b6594a6dd663bbedcc1
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-24 17:08:38 +0100

    add path checking; add configfilepath argument

--- githubanalysis/processing/setup_github_auth.py
+++ githubanalysis/processing/setup_github_auth.py
@@ -1,13 +1,15 @@
 """ Set up authenticated access to github."""
 
 import configparser
+from pathlib import Path
 from github import Github
 
 
-def setup_github_auth():
+def setup_github_auth(configfilepath):
     """
     Authenticates with Github API using user-generated config.cfg file contents; sets per_page to 100 items.
-
+    :param configfilepath: file path of config.cfg file
+    :type str:
     :returns: `ghlink`
     :type github.MainClass.Github:
 
@@ -16,11 +18,15 @@ def setup_github_auth():
     TODO.
     """
 
-    # set up github access token with github package:
-    config = configparser.ConfigParser()
-    config.read('../config.cfg')
-    config.sections()
+    # check input (using separate variable to avoid overwriting it as pathlib Path type)
+    pathchecker = Path(configfilepath)
+    if pathchecker.exists() == False:
+        raise OSError('Config file does not exist at that path.')
 
+    # read config file and pull out access token details
+    config = configparser.ConfigParser()
+    config.read(configfilepath)
+    config.sections()
     access_token = config['ACCESS']['token']
 
     try:
@@ -29,6 +35,7 @@ def setup_github_auth():
             per_page=100
         )  # type(ghlink) is github.MainClass.Github; ghlink.per_page should return 100
 
+        print('GH link opened')
         return ghlink
 
     except RuntimeError:
