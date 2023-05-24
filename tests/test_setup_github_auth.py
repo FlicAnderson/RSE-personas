d2a4048519643e2eecf53315884e425208ee7698
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-24 18:39:08 +0100

    mark tests xfail since the setup_github_auth() needs valid config.cfg file which is not uploaded remotely; these tests will be expected to fail on remote workflow pytest run

--- tests/test_setup_github_auth.py
+++ tests/test_setup_github_auth.py
@@ -4,25 +4,26 @@ import pytest
 from pathlib import Path
 from githubanalysis.processing import setup_github_auth
 
+@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 def test_config_file_not_exists():
     # test config file exists where expected
-    # NOTE: this function doesn't actually touch any of the setup_github_auth.py code
     fakeconfigfilepath = 'githubanalysis/fakefolder/config.cfg'
 
     with pytest.raises(OSError):
         setup_github_auth.setup_github_auth(configfilepath=fakeconfigfilepath)
 
-
+#@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 # def test_config_file_format():
     # should test what happens if the file doesn't have correct format (e.g. heading not [ACCESS]?)
     # should get KeyError if the file is located correctly but incorrect format.
 
-
+#@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 # def test_config_file_GH_invalid():
     # should test what happens if the file doesn't contain GH-approved-valid access token.
     # should get RuntimeError if the file is located correctly and correct format but it's not a valid GH one.
     # could create text file in correct format and 'generate' incorrect hash. ?? might cause blocking tho - check GH API guidance ??
 
+@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 def test_per_page_25():
     # test per_page attribute value changes with user input as expected
 
@@ -30,7 +31,7 @@ def test_per_page_25():
 
     assert ghlink.per_page == 25
 
-
+@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 def test_per_page_default100():
     # test per_page attribute value is set to 100 as expected (default defined as 100 in setup_github_auth)
 
@@ -39,7 +40,7 @@ def test_per_page_default100():
     assert ghlink.per_page == 100
 
 
-
+@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
 def test_per_page_exceed100():
     # test per_page attribute value max is 100 in GH API, so don't accept this!
 
