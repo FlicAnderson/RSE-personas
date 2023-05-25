4cf95fa5f8aa36e5b60f78cc615f537d61786e07
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-25 12:43:02 +0100

    create test script for get_all_pages_issues

---
+++ tests/test_get_all_pages_issues.py
@@ -0,0 +1,8 @@
+""" Test function for getting all pages of items for issues for a given repository. """
+
+import pytest
+import githubanalysis.processing.get_all_pages_issues as issuepages
+
+#@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
+#def test_():
+    #
