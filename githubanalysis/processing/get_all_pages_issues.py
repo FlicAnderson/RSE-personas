43f1b206b06c3f9d5ca27266e230debb862e37b9
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-25 13:36:08 +0100

    add functionality comments as guide to writing

--- githubanalysis/processing/get_all_pages_issues.py
+++ githubanalysis/processing/get_all_pages_issues.py
@@ -16,4 +16,14 @@ def get_all_pages_issues(repo_name):
     TODO.
     """
 
+    # use requests.get().links to find number of last page:
+    # ?loop through making request for each page with link e.g. for i in maxpgs: requests.get(f'...{repo_name}...page={i}')
+        # save out each set of json
+    # merge the jsons into ONE json (? https://pypi.org/project/jsonmerge/)
+    # ensure it returns out as all_issues
+
+    # check json input contains content?
+    # check for specific fields being there?
+    # check merged json doesn't contain duplicated content (e.g. set i repeated if iterator not reset etc.)
+
     # get_last_page_links a = requests.get(url=f'https://api.github.com/repos/{username}/{repo_name}/issues', params={'per_page': 100}).links
