28d2ea136cbfc512f6db3548edd40633217af7c0
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-30 14:57:41 +0100

    use pygithub get_issues() instead of DIY function using requests.get(); add verbose param; add issue_state param; add counts and df shape printing if verbose

--- githubanalysis/processing/get_all_pages_issues.py
+++ githubanalysis/processing/get_all_pages_issues.py
@@ -1,15 +1,21 @@
 """ Get all data from all pages of issues for a github repo."""
 import pandas as pd
-import requests
+import githubanalysis.processing.get_repo_connection as ghconnect
 
 
-def get_all_pages_issues(repo_name, per_pg=100):
+def get_all_pages_issues(repo_name, per_pg=100, issue_state='all', verbose=True):
     """
     Obtains all fields of data from all pages for a given github repo `repo_name`.
-    :param ???: cleaned `repo_name` string without github url root or trailing slashes.
+    :param repo_name: cleaned `repo_name` string without github url root or trailing slashes.
     :type str:
-    :returns: `issues_data` dataframe(?or github object?)
-    :type ???:
+    :param per_pg: how many issues per GitHub API page to request. Default: 100
+    :type int:
+    :param issue_state: one of 'open', 'closed' or 'all'. GitHub issue API status options. Default: 'all'
+    :type str:
+    :param verbose: whether to print out issue data dimensions and counts. Default: True
+    :type bool:
+    :returns: `all_issues` pd.DataFrame containing 30 fields per issue for given repo `repo_name`.  
+    :type DataFrame:
 
     Examples:
     ----------
@@ -33,38 +39,27 @@ def get_all_pages_issues(repo_name, per_pg=100):
 
     # # use requests.get().links to find number of last page:
 
-    urlparam = f"https://api.github.com/repos/{repo_name}/issues"
-
-    links = requests.get(url=urlparam, params={'per_page' : per_pg}).links
-
-    # ERROR if there's no 'last'. In this case, just take
-    # figure out how many pages there are.
-    last_pg = links['last']['url'].split("&page=")[1] # pull off the number of last page
-    # last_pg should be number (convert from returned string I guess?)
-
-    # pull json for ALL pages
-    pg_url = links['last']['url'].split("&page=")[0] # pull out the part before pagenumber
+    repo_con = ghconnect.get_repo_connection(repo_name)  # create gh repo object to given repo
+    # contains:  #ghlink = ghauth.setup_github_auth() with config path default to '../config' & per_page=100
 
     store_pgs = []
 
-    for i in range(int(last_pg)):  # starts at 0
-        url_num = i + 1
-        # print(i+1) # 1, 2 for i in range(last_pg = 2)
-        tmp_pg_results = requests.get(f"{pg_url}&page={url_num}").json()
-        # counter = i
-        if i == 0:
-            print("first run", url_num, ":", len(tmp_pg_results), type(tmp_pg_results))
-            store_pgs.extend(tmp_pg_results)  # for first set, create store_pgs
-        else:
-            print("subsequent", url_num, ":", len(tmp_pg_results), type(tmp_pg_results))
-            store_pgs.extend(tmp_pg_results)  # for subsequent sets, extend (add to) store_pgs
+    # without `state='all'` you only get open issues.
+    for page in repo_con.get_issues(state=issue_state):
+        store_pgs.append(page._rawData)
 
     # store_pgs holds items in list.
       # Can pull items out using indices e.g. url of 'first' issue: store_pgs[0]['url']
 
-    print("Total responses:", len(store_pgs))
+    if verbose = True:
+        print("Total responses:", len(store_pgs))
 
-    issue_pgs_df = pd.DataFrame(store_pgs)
+    all_issues = pd.DataFrame(store_pgs)
+
+    if verbose = True:
+        print("Shape of data:", all_issues.shape)
+        print("Issue state counts:", all_issues.state.value_counts())
+
     return (issue_pgs_df)
 
     # relevant fields: 'url', 'number', 'assignee'/'assignees', 'created_at', 'closed_at',
