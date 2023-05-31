d5f184e374fc42278fd3c7274aabb1a8d5efdad7
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-05-31 18:23:31 +0100

    add config_path and per_pg to arguments of get_all_pages_issues(); fix linter issues; remove unnecessary comments; fix defstring type format

--- githubanalysis/processing/get_all_pages_issues.py
+++ githubanalysis/processing/get_all_pages_issues.py
@@ -1,45 +1,30 @@
-""" Get all data from all pages of issues for a github repo."""
+""" Get all data from all pages of issues for a GitHub repo."""
 import pandas as pd
 import githubanalysis.processing.get_repo_connection as ghconnect
 
 
-def get_all_pages_issues(repo_name, per_pg=100, issue_state='all', verbose=True):
+def get_all_pages_issues(repo_name, config_path='githubanalysis/config.cfg', per_pg=100, issue_state='all', verbose=True):
     """
     Obtains all fields of data from all pages for a given github repo `repo_name`.
     :param repo_name: cleaned `repo_name` string without github url root or trailing slashes.
-    :type str:
-    :param per_pg: how many issues per GitHub API page to request. Default: 100
-    :type int:
+    :type: str
+    :param config_path: file path of config.cfg file. Default=githubanalysis/config.cfg'.
+    :type: str
+    :param per_pg: number of items per page in paginated API requests. Default=100, overwrites GitHub default 30.
+    :type: int
     :param issue_state: one of 'open', 'closed' or 'all'. GitHub issue API status options. Default: 'all'
-    :type str:
+    :type: str
     :param verbose: whether to print out issue data dimensions and counts. Default: True
-    :type bool:
+    :type: bool
     :returns: `all_issues` pd.DataFrame containing 30 fields per issue for given repo `repo_name`.
-    :type DataFrame:
+    :type: DataFrame
 
     Examples:
     ----------
     TODO.
     """
 
-    # use requests.get().links to find number of last page:
-
-    # ?loop through making request for each page with link e.g. for i in maxpgs: requests.get(f'...{repo_name}...page={i}')
-        # save out each set of json
-        # un-nest/flatten json parts w/ json_normalize?
-    # merge the jsons into ONE json (? https://pypi.org/project/jsonmerge/)
-    # ensure it returns out as all_issues
-
-    # checking/testing:
-        # validate json returned?
-        # check json input contains content?
-        # check for specific fields being there?
-        # check merged json doesn't contain duplicated content (e.g. set i repeated if iterator not reset etc.)
-
-
-    # # use requests.get().links to find number of last page:
-
-    repo_con = ghconnect.get_repo_connection(repo_name)  # create gh repo object to given repo
+    repo_con = ghconnect.get_repo_connection(repo_name, config_path, per_pg)  # create gh repo object to given repo
     # contains:  #ghlink = ghauth.setup_github_auth() with config path default to '../config' & per_page=100
 
     store_pgs = []
@@ -47,9 +32,10 @@ def get_all_pages_issues(repo_name, per_pg=100, issue_state='all', verbose=True)
     # without `state='all'` you only get open issues.
     for page in repo_con.get_issues(state=issue_state):
         store_pgs.append(page._rawData)
+        # this is currently 'access to a protected member _rawData of a class'... :s
 
     # store_pgs holds items in list.
-      # Can pull items out using indices e.g. url of 'first' issue: store_pgs[0]['url']
+        # Can pull items out using indices e.g. url of 'first' issue: store_pgs[0]['url']
 
     if verbose:
         print("Total responses:", len(store_pgs))
@@ -60,9 +46,10 @@ def get_all_pages_issues(repo_name, per_pg=100, issue_state='all', verbose=True)
         print("Shape of data:", all_issues.shape)
         print("Issue state counts:", all_issues.state.value_counts())
 
-    return (all_issues)
+    return all_issues
 
     # relevant fields: 'url', 'number', 'assignee'/'assignees', 'created_at', 'closed_at',
-    # ... 'pull_request' (contains url of PR if so), 'title', 'repository_url', 'labels' (bug, good first issue etc), 'state' (open/closed), 'user' (created issue)
+    # ... 'pull_request' (contains url of PR if so), 'title', 'repository_url',
+    # ... 'labels' (bug, good first issue etc), 'state' (open/closed), 'user' (created issue)
 
     # todo: add optional informative message about how many issues, pages etc?
