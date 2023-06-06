4bf94f9f5794eaaa9940567a5e81574cd5e117e9
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-06-06 19:13:41 +0100

    try checking types are datetime.datetime or pd.Timestamp

--- githubanalysis/processing/calc_issue_close_time.py
+++ githubanalysis/processing/calc_issue_close_time.py
@@ -1,22 +1,26 @@
 """ Function to calculate time in days to close issue for given repo."""
 
-# calculate close time from DF rather than in loop.
-# This function needs more detail and probably isn't very robust just now.
+import pandas as pd
+import datetime
 
 
 def calc_issue_close_time(created_at, closed_at):
     """
     Takes repo dataset file (e.g. issues data) in json and reads in as pd.DataFrame object.
     :param created_at: creation time of issue; timestamp (ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ) (e.g. "created_at": "2011-04-10T20:09:31Z")
-    :type: str
+    :type: datetime
     :param closed_at: closure time of issue; timestamp (ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ) (e.g. "closed_at": "2013-02-12T13:22:01Z")
-    :type: str
+    :type: datetime
     :return time_diff: time_diff in days to close GitHub issue.
-    :type: str
+    :type: ?str
     """
 
-    # check closed_at and created_at are datetimes
+    # todo: check closed_at and created_at are datetimes
+    if (type(created_at) or type(closed_at)) is not (datetime.datetime or pd.Timestamp):
+        raise TypeError("Input dates are not of type date. Please convert these.")
 
     time_diff = closed_at - created_at
+
+    # todo: make precision of return an argument
 
     return time_diff.days
