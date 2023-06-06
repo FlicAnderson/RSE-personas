58692b8560ee56d2e499ed36f7bf74356d5125c8
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-06-06 17:28:09 +0100

    add empty stub for testing read_in_repo_data()

---
+++ tests/test_read_in_repo_data.py
@@ -0,0 +1,10 @@
+""" Test function for reading file of GitHub repo data in as dataframes. """
+
+#import pytest
+#import pandas as pd
+#from pathlib import Path
+
+#import githubanalysis.processing.get_all_pages_issues as issuepages
+#import githubanalysis.processing.write_out_repo_data as writeout
+#import githubanalysis.processing.read_in_repo_data as readin
+
