2f996acd00a5e0b99cdba8aaec33968f4fd4c5cc
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-06-12 16:22:27 +0100

    add return_in param for specifying time unit to return value of time_diff in (e.g. decimal_days, whole_days, days, hours, minutes, seconds)

--- githubanalysis/processing/calc_issue_close_time.py
+++ githubanalysis/processing/calc_issue_close_time.py
@@ -4,23 +4,46 @@ import pandas as pd
 import datetime
 
 
-def calc_issue_close_time(created_at, closed_at):
+def calc_issue_close_time(created_at, closed_at, return_in='decimal_days'):
     """
     Takes repo dataset file (e.g. issues data) in json and reads in as pd.DataFrame object.
     :param created_at: creation time of issue; timestamp (ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ) (e.g. "created_at": "2011-04-10T20:09:31Z")
     :type: datetime
     :param closed_at: closure time of issue; timestamp (ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ) (e.g. "closed_at": "2013-02-12T13:22:01Z")
     :type: datetime
+    :param return_in: granularity to return time difference in ('decimal_days', 'whole_days', 'hours', 'minutes', 'seconds'). Default: 'decimal_days' e.g. 2.7097...
+    :type: str
     :return time_diff: time_diff in days to close GitHub issue.
     :type: ?str
     """
 
     # todo: check closed_at and created_at are datetimes
-    if (type(created_at) or type(closed_at)) is not (datetime.datetime or pd.Timestamp):
-        raise TypeError("Input dates are not of type date. Please convert these.")
+    #if (type(created_at) or type(closed_at)) is not (datetime.datetime or pd.Timestamp):
+    #    raise TypeError("Input dates are not of type date. Please convert these.")
 
-    time_diff = closed_at - created_at
+    if return_in not in ['decimal_days', 'whole_days', 'hours', 'minutes', 'seconds']:
+        raise ValueError("`return_in` parameter must be one of: 'decimal_days', 'whole_days', 'hours', 'minutes', 'seconds'.")
+
+    time_diff = pd.Timedelta(closed_at - created_at)
 
     # todo: make precision of return an argument
 
-    return time_diff.days
+    try:
+
+        if return_in == 'decimal_days':
+            return ((time_diff.total_seconds()/60)/60)/24  # returns days in decimal fraction e.g. '2.7097...' is 2 days 17 hours 2 mins etc.
+
+        elif return_in == 'whole_days':
+            return time_diff.days  # returns whole days e.g. '2' where time_diff is 2 days 17 hours 2 minutes etc.
+
+        elif return_in == 'hours':
+            return (time_diff.total_seconds()/60)/60
+
+        elif return_in == 'minutes':
+            return time_diff.total_seconds()/60
+
+        else:
+            return time_diff.total_seconds()
+
+    except:
+        raise RuntimeError('Calculating time_diff has failed.')
