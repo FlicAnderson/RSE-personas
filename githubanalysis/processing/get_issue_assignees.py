6fb96d4e41fd0a6c7cadd616afdaead98189e095
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-06-13 20:09:28 +0100

    add verbose=True param and pull printing df.shape into that; remove printing 'team' df.assigned_devs.value_counts()

--- githubanalysis/processing/get_issue_assignees.py
+++ githubanalysis/processing/get_issue_assignees.py
@@ -3,24 +3,29 @@
 import pandas as pd
 
 
-def get_issue_assignees(repo_data_df):
+def get_issue_assignees(repo_data_df, verbose=True):
     """
     Takes repo issue ticket data (pd.DataFrame object), and creates new column of assignee data pulled out from 'assignees' column.
     :param repo_data_df: data to parse assignees data for
     :type: pd.DataFrame
+    :param verbose: print data before/after dimensions and dev assignment counts?
+    :type: bool
     :return repo_data_df: input dataframe with assignee data clearly appended in 'assigned_devs' column as lists.
     :type: pd.DataFrame
     """
 
     if repo_data_df.empty:  # if read_in hasn't worked and dataframe is empty
         print(f"Dataframe is empty.")
-    else:
+
+    if verbose:
         print(f"Dataframe has original dimensions: {repo_data_df.shape}.")
 
     repo_data_df['assigned_devs'] = repo_data_df[['assignees']].applymap(
         lambda x: [x.get('login') for x in x]
     )
 
-    print(f"Dataframe has been updated with 'assigned_devs' info and now has dimensions: {repo_data_df.shape}.")
+    if verbose:
+        print(f"Dataframe has been updated with 'assigned_devs' info and now has dimensions: {repo_data_df.shape}.")
+        #print(repo_data_df['assigned_devs'].value_counts(dropna=False))
 
     return repo_data_df
