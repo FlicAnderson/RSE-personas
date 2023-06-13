0dd85cbdb462ceb0ba06dd3c277d864e7c9d18be
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-06-13 15:37:03 +0100

    add pseudocode for calc_per_dev_mean_close_time()

---
+++ githubanalysis/analysis/calc_per_dev_mean_close_time.py
@@ -0,0 +1,32 @@
+""" Function to calculate average issue close times per assigned dev username from issue ticket data for a repo."""
+
+import pandas as pd
+
+
+def calc_per_dev_mean_close_time(repo_data_df):
+    """
+    Takes repo issue ticket data (pd.DataFrame object), and creates new column of assignee data pulled out from 'assignees' column.
+    :param repo_data_df: closed issue ticket data to calculate issue close_time data for (requires 'assigned_devs' column)
+    :type: pd.DataFrame
+    :return repo_data_df:
+    :type: pd.DataFrame
+    """
+
+    # check input df 'repo_data_df' isn't empty
+    # check df has 'close_time' column present (ie calc_issue_close_time() has been run)
+    # check 'assigned_devs' column present (ie get_issue_assignees() has been run)
+
+    # get all unique dev usernames from closed issue tickets -> 'dev_list'
+
+    # for each dev in dev_list:
+    #   identify each issue where dev is assigned
+
+        #   get close_time for issue
+
+        #   add dev, close_time and issue number and repo_number to df 'all_devs_close_times'
+        #   calculate mean from all_devs_close_times for dev
+
+        #   return (dev, per_dev_mean_close_time) for dev to output (+/- repo_name?)
+
+    # print each (dev, per_dev_mean_close_time) tuple if verbose
+    # return df of devs and per_dev_mean_close_times.
