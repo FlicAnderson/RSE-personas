28922a036e27b4aefb418122e8478c60c192505b
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-07-27 17:01:43 +0100

    remove unnecessary files, not developing this as function anymore

--- githubanalysis/processing/issues_json_to_df.py
+++
@@ -1,8 +0,0 @@
-""" Function to shift json issues output from oneinto a dataframe. """
-
-import json
-import pandas as pd
-
-#def issues_json_to_df(all_issues):
-    # take in all json format issues, transform into pandas dataframe.
-
--- tests/test_issues_json_to_df.py
+++
@@ -1,7 +0,0 @@
-""" Test issues_json_to_df() function. """
-
-import pytest
-
-#@pytest.mark.xfail(reason="Fails remotely: relies on GH config file")
-#def test_():
-    #
