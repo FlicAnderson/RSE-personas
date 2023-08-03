e3082fe287270836fa564579c995dc7a93f916c7
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-08-03 18:21:25 +0100

    add requests code to retry with waits and timeout to handle 202 status while GH API is caching data; currently includes recursion if api_response = 202, this could cause issues; returns empty dfs if no returns (204) or other issue

--- githubanalysis/processing/get_contributor_commits_stats.py
+++ githubanalysis/processing/get_contributor_commits_stats.py
@@ -2,11 +2,14 @@
 
 import pandas as pd
 import requests
+from requests.adapters import HTTPAdapter, Retry
+import logging
+
 
 
 def get_contributor_commits_stats(repo_name, verbose=True):
     """
-    Takes repo name, creates new dataframe of contributor commit activity (pd.DataFrame object) for that repository.
+    Takes repo name, creates new dataframe of total contributor commit activity (pd.DataFrame object) for the repository.
     :param repo_name: cleaned `repo_name` string without GitHub url root or trailing slashes.
     :type: str
     :param verbose: print status info? Default= true
@@ -15,21 +18,60 @@ def get_contributor_commits_stats(repo_name, verbose=True):
     :type: pd.DataFrame
 
     This returns statistics on contributor stats, rather than returning ALL commits for contributors.
+    See more at the GH API docs: https://docs.github.com/en/rest/metrics/statistics?apiVersion=2022-11-28#get-all-contributor-commit-activity
     """
 
     # handle API responses:
     base_contributor_stats_url = f"https://api.github.com/repos/{repo_name}/stats/contributors"
-    api_response = requests.get(url=base_contributor_stats_url)
+    # approach via: https://stackoverflow.com/a/35636367
+
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)  # might be able to remove this as it doesn't affect the requests code
+
+    s = requests.Session()
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[202, 502, 503, 504])
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+
+    api_response = s.get(url=base_contributor_stats_url, timeout=10)
 
     if api_response.status_code == 200 and verbose:
         print(f'API response status "OK": {api_response}')
 
+        try:
+            # pull data as json then convert to pandas dataframe for ease of use
+            contributor_stats_info = api_response.json()
+            contributor_commits_stats = pd.DataFrame.from_dict(contributor_stats_info)
+
+            if verbose:
+                print(contributor_commits_stats.shape)
+
+            if len(contributor_commits_stats.columns) == 0:
+                raise Exception(f"Repo {repo_name} contains NO contributor stats.")
+
+            # pull author username out from rest of author data
+            contributor_commits_stats['contributor_author'] = contributor_commits_stats[['author']].apply(
+                lambda x: [x.get('login') for x in x])
+
+            return contributor_commits_stats
+
+        except:
+            print("No contributor stats found")
+            contributor_commits_stats = pd.DataFrame()
+            return contributor_commits_stats
+
     elif api_response.status_code == 202 and verbose:
-        print(f'API response status "Accepted": {api_response}. Data not yet been cached, will retry request shortly.')
-        # WAIT THEN RETRY REQUEST! Based on GH API docs: https://docs.github.com/en/rest/metrics/statistics?apiVersion=2022-11-28#best-practices-for-caching
+        print(f'API response status "Accepted": {api_response}. Data not yet been cached, need to retry request shortly.')
+        # Wait further, then retry...
+        print('Retrying same function recursively; this could go infinitely wrong... Cancel if uncertain tbh!')
+        get_contributor_commits_stats(repo_name, verbose=True)
+
 
     elif api_response.status_code == 204 and verbose:
         print(f'API response status "A header with no content is returned": {api_response}')
+        contributor_commits_stats = pd.DataFrame()
+        return contributor_commits_stats
 
     else:
         print(f'Something else is wrong; API response: {api_response}')
+        contributor_commits_stats = pd.DataFrame()
+        return contributor_commits_stats
