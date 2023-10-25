eb6116d75eab4ba5e83a955707ed1a13bea1aeb9
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-10-25 17:07:58 +0100

    add comments for each eligibility criteria

--- githubanalysis/processing/check_repo_eligibility.py
+++ githubanalysis/processing/check_repo_eligibility.py
@@ -27,3 +27,48 @@ def check_repo_eligibility(repo_name, config_path='githubanalysis/config.cfg', p
     >>> check_repo_eligibility('riboviz/riboviz')
     TODO
     """
+
+    # This function will require output from githubanalysis.processing.summarise_repo_stats() function.
+    # Will return repo_eligible = True if repo meets inclusion/exclusion criteria.
+        # Criteria should be set either in separate config file or as function arguments (ease of use/reproducibility)
+    # This function MAY sit better outside of main analysis 'pipeline'
+        # it could be used to output eligible repo names to separate csv file.
+        # once repo has been established to be eligible, then probably unlikely to need to rerun this on same repo?
+        # Perhaps consider this more as a stand alone tool?
+
+    # Dev Numbers:
+        # repo has > 1 dev
+        # repo has < 1000 devs
+
+    # Commits:
+        # > 100 commits? > 500?
+
+    # Is RS:
+        # (will be TRUE since the gh repo url should have come via zenodocode therefore has associated DOI)
+
+    # Issue Tickets:
+        # uses issue tickets
+        # has > 10 issue tickets
+
+    # Recent Activity:
+        # has commit within last 12 months? 18 months?
+    
+    # Repo Age:
+        # repo is established, >3 years old.
+
+    # Licence:
+        # has open license allowing me to work w/ it
+
+    # Accessibility:
+        # repo is set to public
+
+    # Repo Content:
+        # repo contains code, not just docs or data.
+        # look for file endings.
+
+    # Repo Language:
+        # contains some of: python, (shell?), (R?), java, C, C++, (FORTRAN???)
+
+    # Repo Architecture:
+        # Not HPC only; ideally intended to run on desktops, moderate servers, NOT HPC systems.
+        # exclude any repos with scheduler code ? submission scripts for HPC architecture?
