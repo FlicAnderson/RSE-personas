46ab3a80b51a4a6f5ccb037c908fc2af9d007542
Flic Anderson <Felicity.Anderson@ed.ac.uk>
2023-12-15 16:42:23 +0000

    add new get_zn_ids function

--- zenodocode/get_gh_repo_dataset.py
+++ zenodocode/get_gh_repo_dataset.py
@@ -2,6 +2,7 @@
 
 import sys
 import zenodocode.setup_zenodo_auth as znconnect
+import zenodocode.get_zenodo_ids as zngetids
 import zenodocode.get_gh_urls as zngetghurls
 
 def main():
@@ -13,9 +14,12 @@ def main():
 
     znconnect.setup_zenodo_auth(config_path='zenodocode/zenodoconfig.cfg', verbose=True)
 
+    # get zenodo IDs
+    zngetids.get_zenodo_ids(config_path='zenodoconfig.cfg', per_pg=20, total_records=100, filename='zn_ids', write_out_location='data/', verbose=True)
+
     # get github urls
 
-    zngetghurls.get_gh_urls(config_path='zenodocode/zenodoconfig.cfg', per_pg=100, total_records=1000, filename='gh_urls', write_out_location='data/', verbose=True)
+    #zngetghurls.get_gh_urls(config_path='zenodocode/zenodoconfig.cfg', zenodo_ids_file='data/zn_ids.csv', per_pg=100, total_records=1000, filename='gh_urls', write_out_location='data/', verbose=True)
 
 
 
