bb1f3ab56d5e51eb0f3011362d4d7947f4651071
FlicAnderson <Felicity.Anderson@ed.ac.uk>
2024-02-19 17:42:45 +0000

    replace pd.read_json(url) with pd.DataFrame.from_dict(json) to get API rate limiting from s.get() in the loop; add docstring example; add % issues open/closed stats for info statements

--- githubanalysis/processing/get_all_pages_issues.py
+++ githubanalysis/processing/get_all_pages_issues.py
@@ -43,8 +43,31 @@ class IssueGetter:
 
         Examples:
         ----------
-        TODO.
+        $ python githubanalysis/processing/get_all_pages_issues.py 'Dallinger/Dallinger'
+        
+        INFO:Using commandline argument Dallinger/Dallinger as repo name to retrieve GH issues for. Entered as: Dallinger/Dallinger  
+        INFO:Number of issues returned for repo Dallinger/Dallinger is 6207.  
+        INFO:There are 344 open issues (~6%) and 5863 closed issues (~94%).
+
+        $ python githubanalysis/processing/get_all_pages_issues.py 'sizespectrum/mizer'  
+        INFO:Using commandline argument sizespectrum/mizer as repo name to retrieve GH issues for. Entered as: sizespectrum/mizer  
+        INFO:>> Running issue grab for repo sizespectrum/mizer, in page 1 of 3.  
+        INFO:>> Running issue grab for repo sizespectrum/mizer, in page 2 of 3.  
+        INFO:>> Running issue grab for repo sizespectrum/mizer, in page 3 of 3.  
+        INFO:Number of issues returned for repo sizespectrum/mizer is 281.  
+        INFO:There are 47 open issues (~17%) and 234 closed issues (~83%).
         """
+
+        # get repo_name from commandline if given (accept commandline input) 
+        if len(sys.argv) == 2:
+            repo_name = sys.argv[1]  # use second argv (user-provided by commandline)
+
+            if not isinstance(repo_name, str):
+                raise TypeError("Ensure argument is repository name in string format (e.g. 'repo-owner/repo-name')")
+
+            self.logger.info(f"Using commandline argument {repo_name} as repo name to retrieve GH issues for. Entered as: {sys.argv[1]}")
+        else: 
+            self.logger.debug(f"Repo name is {repo_name}. Getting issues.")
 
         # write-out file setup     
         # get date for generating extra filename info
@@ -60,8 +83,6 @@ class IssueGetter:
         s = requests.Session()
         retries = Retry(total=10, connect=5, read=3, backoff_factor=1.5, status_forcelist=[202, 502, 503, 504])
         s.mount('https://', HTTPAdapter(max_retries=retries))
-
-        self.logger.debug(f"Repo name is {repo_name}. Getting issues.")
 
         # create empty df to store issues data
         all_issues = pd.DataFrame()
@@ -98,8 +119,12 @@ class IssueGetter:
 
                     for i in pg_range: 
                         pg_count += 1
-                        self.logger.debug(f">> Running issue grab for repo {repo_name}, in page {pg_count} of {pages_issues}.")
-                        store_pg = pd.read_json(path_or_buf=f"https://api.github.com/repos/{repo_name}/issues?state={state}&per_page=100&pulls={pulls}&page={i}")
+                        self.logger.info(f">> Running issue grab for repo {repo_name}, in page {pg_count} of {pages_issues}.")
+                        issues_query = f"https://api.github.com/repos/{repo_name}/issues?state={state}&per_page=100&pulls={pulls}&page={i}"
+                        api_response = s.get(url=issues_query, headers=headers)
+                        json_pg = api_response.json()
+                        store_pg = pd.DataFrame.from_dict(json_pg)  # convert json to pd.df
+                          # using pd.DataFrame.from_dict(json) instead of pd.read_json(url) because otherwise I lose rate handling 
                         
                         if len(store_pg.index) > 0:
                              # write out 'completed' page of issues as df to csv via APPEND (use added date filename with reponame inc)
@@ -110,7 +135,8 @@ class IssueGetter:
                 else: # there's no next page, grab all on this page and proceed.
                     pg_count += 1
                     self.logger.debug(f"getting json via request url {issues_url}.")
-                    store_pg = pd.read_json(path_or_buf=issues_url)
+                    json_pg = api_response.json()
+                    store_pg = pd.DataFrame.from_dict(json_pg)
                     all_issues = store_pg
                      # write out the page content to csv via APPEND (use added date filename)
                     all_issues.to_csv(write_out_extra_info, mode='a', index=True, header= not os.path.exists(write_out_extra_info))
@@ -118,25 +144,13 @@ class IssueGetter:
                 self.logger.debug(f"Total number of issues grabbed is {len(all_issues.index)} in {pg_count} page(s).")
                 self.logger.debug(f"Issues data written out to file for repo {repo_name} at {write_out_extra_info}.")
 
+                self.logger.debug(f"There are {all_issues.state.value_counts().open} open issues (~{round(all_issues.state.value_counts(normalize=True).open*100)}%) and {all_issues.state.value_counts().closed} closed issues (~{round(all_issues.state.value_counts(normalize=True).closed*100)}%).")
 
             except Exception as e_issues:
                 self.logger.error(f"Something failed in getting issues for repo {repo_name}: {e_issues}")
                 
         
-        # # handle empty (ie no closed issues case); return empty df
-        # if len(store_pg) == 0: 
-        #     all_issues = pd.DataFrame()
-        # # handle case with closed issues...
-        # else: 
-        #     # store_pgs holds items in list.
-        #     # Can pull items out using indices e.g. url of 'first' issue: store_pgs[0]['url']
-
-        #     print("Total responses:", len(store_pg))
-
-        #     all_issues = pd.DataFrame(store_pg)
-
-        #     print("Shape of data:", all_issues.shape)
-        #     print("Issue state counts:", all_issues.state.value_counts())
+            
 
         #     # check all important columns are present in the df.
         #     wanted_cols = ['url', 'repository_url', 'labels', 'number', 'title', 'state',
@@ -181,12 +195,29 @@ if __name__ == "__main__":
         repo_name = name_clean.repo_name_clean(repo_name)
 
     issues_df = pd.DataFrame()
-
+    # run the main function to get the issues!
     try: 
         issues_df = issues_getter.get_all_pages_issues(repo_name=repo_name, config_path='githubanalysis/config.cfg')
         if len(issues_df) != 0:
             logger.info(f"Number of issues returned for repo {repo_name} is {len(issues_df.index)}.")
+            logger.info(f"There are {issues_df.state.value_counts().open} open issues (~{round(issues_df.state.value_counts(normalize=True).open*100)}%) and {issues_df.state.value_counts().closed} closed issues (~{round(issues_df.state.value_counts(normalize=True).closed*100)}%).")
         else:
             logger.warning("Getting issues did not work, length of returned records is zero.")
     except Exception as e:
         logger.error(f"Exception while running get_all_pages_issues() on repo {repo_name}: {e}")
+
+    # generate filename and try to read file in for comparison.
+    total_issues = pd.DataFrame()
+    try: 
+        current_date_info = datetime.now().strftime("%Y-%m-%d") # run this at start of script not in loop to avoid midnight/long-run issues
+        sanitised_repo_name = repo_name.replace("/", "-")
+        issues_file = f"data/all-issues"
+        issues_file_extra_info = f"{issues_file}_{sanitised_repo_name}_{current_date_info}.csv"
+        total_issues = pd.read_csv(issues_file_extra_info, header=0)
+    except Exception as e:
+        logger.error(f"There's been an exception while trying to read back in data generated by get_all_pages_issues() from {issues_file_extra_info}: {e}")
+
+    try:
+        assert  len(issues_df.index) == len(total_issues.index), f"WARNING! Lengths of returned df ({len(issues_df)}) vs df read in from file ({len(total_issues)}) DO NOT MATCH. Did you append too many records to the gh_urls file??"
+    except AssertionError as e:
+        logger.error(f"The outputs of running the function and reading back in data DO NOT MATCH; {e}")
