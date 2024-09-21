16930414af8b57602517a48127f43155e3a9f6d9
FlicAnderson <s0784624@ed.ac.uk>
2024-09-21 16:20:33 +0100

    remove unnecessary bit

--- utilities/chunker.py
+++ utilities/chunker.py
@@ -13,14 +13,4 @@ def chunker(iterable, size=10):
     """
     for pos in range(0, len(iterable), size):
         yield iterable.iloc[pos : pos + size]
-
-
-# via Andrei Krivoshei at SO: https://stackoverflow.com/a/61798585
-
-
-# this bit
-if __name__ == "__main__":
-    try:
-        chunker(iterable, size=10)
-    except Exception as e:
-        print(f"Exception issue with chunker(): {e}")
+    # via Andrei Krivoshei at SO: https://stackoverflow.com/a/61798585
