cd24739b05f9d285a098bdf78f96bab8d82accf5
FlicAnderson <s0784624@ed.ac.uk>
2024-09-21 12:46:09 +0100

    try fix multiple values for arg 'repo_name' typeerror

--- githubanalysis/processing/get_all_branches_commits.py
+++ githubanalysis/processing/get_all_branches_commits.py
@@ -69,6 +69,6 @@ class AllBranchesCommitsGetter:
         # create empty df to store commits data
         all_commits = pd.DataFrame()
 
-        branches_info = branchgetter.get_branches(self, repo_name=repo_name, config_path=config_path, per_pg=per_pg)
+        branches_info = branchgetter.get_branches(self, repo_name, config_path=config_path, per_pg=per_pg)
 
         print(len(branches_info))
