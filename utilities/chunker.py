e810413b416ebc5c562bc0ba524a22cc89930585
FlicAnderson <s0784624@ed.ac.uk>
2024-09-21 15:25:53 +0100

    try and fix one of undefined name errors

--- utilities/chunker.py
+++ utilities/chunker.py
@@ -1,18 +1,18 @@
 """Function which splits things into chunks."""
 
 
-def chunker(seq, size=10):
+def chunker(iterable, size=10):
     """
-    :param seq: the thing to split (iterable thing)
-    :type: str
+    :param iterable: the thing to split (iterable thing)
+    :type: str or int
     :param size: size of block to chunk seq into/
     :type: int
     :returns: 'chunk'
     :type: ???
 
     """
-    for pos in range(0, len(seq), size):
-        yield seq.iloc[pos : pos + size]
+    for pos in range(0, len(iterable), size):
+        yield iterable.iloc[pos : pos + size]
 
 
 # via Andrei Krivoshei at SO: https://stackoverflow.com/a/61798585
@@ -21,6 +21,6 @@ def chunker(seq, size=10):
 # this bit
 if __name__ == "__main__":
     try:
-        chunker(seq, size)
+        chunker(iterable, size=10)
     except Exception as e:
         print(f"Exception issue with chunker(): {e}")
