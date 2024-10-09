f21147875e21d22af570db712fb228f00308284f
FlicAnderson <s0784624@ed.ac.uk>
2024-10-09 21:06:29 +0100

    draft workflow logic including skipping

--- workflow_preparation.py
+++ workflow_preparation.py
@@ -1,26 +1,50 @@
 from zenodocode.get_zenodo_ids import get_zenodo_ids as fetch_zs
 from zenodocode.get_gh_urls import GhURLsGetter
+import pandas as pd
+from enum import Enum, auto
 
 
 def get_zenodo_ids(n_records_to_fetch: int, config) -> list[int]:
-    return fetch_zs(auth=config["ACESS"]["token"], total_records=n_records_to_fetch)  # type: ignore
+    return fetch_zs(auth=config["ACCESS"]["token"], total_records=n_records_to_fetch)  # type: ignore
 
 
-def get_repo_urls(
-    zenodo_IDs: list[int],
+def get_gh_repo_urls(zenodo_IDs: list[int]):
+    getter = GhURLsGetter()
+    return getter.get_gh_urls(zenodo_ids=zenodo_IDs)
+
+
+"""
+TODO:
+- [ ] finish summarise_gh_repos
+- [ ] We want to be able to resume from existing files:
+   - [ ] This requires error handling: 
+   - [ ]
+"""
+
+
+class SkipTo(Enum):
+    NONE = auto()
+    REPO_UYRLS = auto()
+    SUMMARISED_GH_URLS = auto()
+
+
+def prepare_workflow(
+    n_records_to_fetch,
+    config,
+    skip_to: SkipTo = SkipTo.NONE,
+    starting_data=None,
 ):
-    return GhURLsGetter.get_gh_urls(self, zenodo_ids=zenodo_IDs, out_filename="gh_urls")
-
-
-def prepare_workflow(n_records_to_fetch, config):
-    config = load_config_file()
-
-    ids = get_zenodo_ids(n_records_to_fetch, config)
-
-    repo_urls = get_repo_urls(ids)
-
-    gh_repo_urls = filter_gh_repo_urls(repo_urls)
-
-    summarised_gh_repos = summarise_gh_repos(gh_repo_urls)
+    if skip_to.value < SkipTo.SUMMARISED_GH_URLS.value:
+        if skip_to.value < SkipTo.REPO_UYRLS.value:
+            if skip_to.value < SkipTo.NONE.value:
+                ids = get_zenodo_ids(n_records_to_fetch, config)
+            else:
+                ids = starting_data
+            gh_repo_urls = get_gh_repo_urls(ids)
+        else:
+            gh_repo_urls = starting_data
+        summarised_gh_repos = summarise_gh_repos(gh_repo_urls)
+    else:
+        summarised_gh_repos = starting_data
 
     return summarised_gh_repos
