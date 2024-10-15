d3784d08d6013c55869d363f601e8834354804cd
FlicAnderson <s0784624@ed.ac.uk>
2024-10-15 21:17:23 +0100

    read json test data files

--- tests/test_get_all_branches_commits.py
+++ tests/test_get_all_branches_commits.py
@@ -1,7 +1,7 @@
 """Testing for core commits-grab code."""
 
 # import requests
-# import json
+import json
 
 # from githubanalysis.processing.get_all_branches_commits import AllBranchesCommitsGetter
 
@@ -11,4 +11,22 @@
 #     # Act:
 
 #     # Assert:
-#     pass
+
+rawjson = "/tests/testdata/raw-commits__all-branches-commits_FlicAnderson-peramagroon_2024-10-15.json"
+deduplicatedjson = "/tests/testdata/deduplicated-commits__all-branches-commits_FlicAnderson-peramagroon_2024-10-15_deduplicated.json"
+
+with open(rawjson) as f1:
+    rawjsondata = json.load(f1)
+    f1.close
+
+with open(deduplicatedjson) as f2:
+    deduplicatedjsondata = json.load(f2)
+    f2.close
+
+
+# def test_get_all_branches_commits_success():
+#     # Arrange:
+
+#     # Act:
+
+#     # Assert:
