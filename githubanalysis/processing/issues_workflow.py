c00033b666d93e06bd5e1a36a05e94dd1b0e3fc7
FlicAnderson <s0784624@ed.ac.uk>
2024-10-21 19:43:03 +0100

    use check_repo_has_issues() and attempt to check for and handle lack of those; use rate-limit-handling and retrying run functions in new get_issues() function; TODO out next steps tasks in issues workflow; good progress towards #76 but need to handle zero issues option (has_issues =true, but N issues =0) and later return issues data as df instead of json and writeout

--- githubanalysis/processing/issues_workflow.py
+++ githubanalysis/processing/issues_workflow.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 import utilities.get_default_logger as loggit
 from utilities.check_gh_reponse import UnexpectedAPIError
-from githubanalysis.processing.get_all_pages_issues import IssueGetter
+from githubanalysis.processing.get_all_pages_issues import IssueGetter, NoIssuesError
+from utilities.check_gh_reponse import raise_if_response_error, run_with_retries
 
 
 class RunIssues:
@@ -46,15 +47,77 @@ class RunIssues:
         self.repo_name = repo_name
         self.write_read_location = write_read_location
 
-    def check_repo_valid(self):
-        pass
-
-    def check_repo_has_issues(self):
-        pass
+    def check_repo_valid(self) -> bool:
+        issuesgetter = IssueGetter(
+            repo_name=self.repo_name,
+            in_notebook=self.in_notebook,
+            config_path=self.config_path,
+            logger=self.logger,
+        )
+        try:
+            repo_has_issues = issuesgetter.check_repo_has_issues(
+                repo_name=self.repo_name
+            )
+            assert repo_has_issues, f"There are NO issues enabled for repo {self.repo_name}. Cannot obtain issues data for this repo."
+            return repo_has_issues
+        except NoIssuesError as e:
+            self.logger.error(f"Error: Issues are not enabled for this repo: {e}")
+            return False
 
     def check_existing_formatted_issues(self):
         pass
 
+    def get_issues(self):
+        issuesgetter = IssueGetter(
+            repo_name=self.repo_name,
+            in_notebook=self.in_notebook,
+            config_path=self.config_path,
+            logger=self.logger,
+        )
+
+        # main issue getting function:
+        all_issues = issuesgetter.get_all_pages_issues(repo_name=self.repo_name)
+        return all_issues
+
     def run_all_issues(self):  # -> pd.DataFrame:
-        pass
-        # return processed_issues
+        self.logger.info(f"Checking whether repo {self.repo_name} has issues enabled.")
+        worth_running = self.check_repo_valid()
+
+        if worth_running:
+            # DO ISSUE GETTING THINGS
+
+            self.logger.info(
+                f"Running run_all_issues() to get all issues data for repo {self.repo_name}."
+            )
+
+            all_issues = self.get_issues()
+
+            return all_issues
+
+            # TODO:
+            # process issues data
+
+            # ?? column handling
+
+            # all_issues  # JSON TO DATAFRAME
+
+            # if processed_issues is None or processed_issues.empty:
+            #     raise pd.errors.EmptyDataError(
+            #         "Frame is None or pd.DataFrame is empty; perhaps no issues?"
+            #     )
+
+            # log retyping of json to df
+
+            #
+
+            # Write out to CSVs
+            # log writeout
+
+            # return processed issues data (pd.df format)
+            # return processed_issues
+
+        else:
+            # EXIT TO NEXT OR SOMETHING? NoIssuesError has been invoked...
+            self.logger.info(
+                f"There are no issues for repo {self.repo_name}; workflow not run."
+            )
