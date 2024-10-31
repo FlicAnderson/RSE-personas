748985fc31c14083c871b2195d888b074bf1c540
FlicAnderson <s0784624@ed.ac.uk>
2024-10-31 16:27:58 +0000

    rewrite authentication code

--- zenodocode/setup_zenodo_auth.py
+++ zenodocode/setup_zenodo_auth.py
@@ -1,58 +1,28 @@
 """Set up authenticated access to Zenodo API."""
 
 import configparser
-import requests
-from pathlib import Path
+import os
 
 
-def setup_zenodo_auth(config_path="zenodocode/zenodoconfig.cfg", verbose=True):
+def setup_zenodo_auth(config_path: str = "zenodocode/zenodoconfig.cfg") -> str:
     """
     Authenticates with Zenodo API using user-generated zenodoconfig.cfg file contents.
 
     :param config_path: file path of config.cfg file. Default='zenodocode/zenodoconfig.cfg'.
     :type: str
-    :param verbose: return status info. Default: True
-    :type: bool
-    :returns: zenodo_response
-    :type: requests.models.Response
+    :returns: zenodo config token string
+    :rtype: str
 
-    Examples:
-    ----------
-    TODO.
     """
 
-    # check config filepath input (using separate variable to avoid overwriting it as pathlib Path type)
-    pathchecker = Path(config_path)
-    if pathchecker.exists() is False:
-        raise OSError(
-            f"Config file does not exist at path: {config_path} from current location {Path(__file__).resolve().parent}"
-        )
-
-    # read config file and pull out access token details
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    config.sections()
-
+    if not os.path.exists(config_path):
+        raise RuntimeError("Config file does not exist at path:", config_path)
     try:
-        access_token = config["ACCESS"]["token"]
+        # read config file and pull out access token details
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        return config["ACCESS"]["token"]
     except:
-        raise KeyError("Config file access token info not correct somehow.")
-
-    try:
-        zenodo_response = requests.get(
-            url="https://zenodo.org/api/deposit/depositions",
-            params={"access_token": access_token},
-        )  # type(zenodo_response) should be requests.models.Response
-
-        assert zenodo_response.status_code == requests.codes.ok
-        zenodo_response.raise_for_status()
-
-        if verbose:
-            print("Zenodo API request OK")
-
-        return zenodo_response, access_token
-
-    except RuntimeError:
-        print(
+        raise RuntimeError(
             "Zenodo authentication failed. Check config file format and permissions in your zenodo account."
         )
