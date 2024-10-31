035c6b865065d501a205fe610a040d73bbb31dee
FlicAnderson <s0784624@ed.ac.uk>
2024-10-31 18:54:52 +0000

    remove main; use run_with_retries; update

--- zenodocode/get_gh_urls.py
+++ zenodocode/get_gh_urls.py
@@ -6,10 +6,12 @@ from requests.adapters import HTTPAdapter, Retry
 import pandas as pd
 import logging
 import math
-from datetime import datetime
+import datetime
 
 import utilities.chunker as chunker
 import utilities.get_default_logger as loggit
+import zenodocode.setup_zenodo_auth as znauth
+from zenodocode.check_zn_response import run_with_retries, raise_if_response_error
 
 
 class GhURLsGetter:
@@ -18,7 +20,8 @@ class GhURLsGetter:
 
     def __init__(
         self,
-        logger: logging.Logger = None,
+        config_path: str,
+        logger: None | logging.Logger = None,
         in_notebook=False,
     ) -> None:
         if logger is None:
@@ -31,34 +34,40 @@ class GhURLsGetter:
         else:
             self.logger = logger
 
+        self.s = requests.Session()
+        retries = Retry(
+            total=10,
+            connect=5,
+            read=3,
+            backoff_factor=1,
+            status_forcelist=[202, 502, 503, 504],
+        )
+        self.s.mount("https://", HTTPAdapter(max_retries=retries))
+        self.zn_token = znauth.setup_zenodo_auth(config_path=config_path)
+        self.config_path = config_path
+        self.in_notebook = in_notebook
+        # write-out file setup
+        self.current_date_info = datetime.datetime.now().strftime(
+            "%Y-%m-%d"
+        )  # run this at start of script not in loop to avoid midnight/long-run commits
+
     def get_gh_urls(
         self,
         zenodo_ids: list[int],
         out_filename="gh_urls",
         write_out_location="data/",
-        verbose=True,
     ):
         """
             Read in csv of zenodo record IDs for software records and query; pull out GitHub urls from metadata;
             save these records out into a csv file and return all records in a dataframe.
             Logging output to file get_gh_urls_logs.txt
 
-            :param config_path: file path of zenodoconfig.cfg file. Default=zenodocode/zenodoconfig.cfg'.
-            :type: str
-            :param per_pg: number of items per page in paginated API requests. Default=20.
-            :type: int
-            :param total_records: total number of zenodo records to iterate through to pull github URLS from if present. Default=100.
-            :type: int
-            :param in_filename: name of CSV file to read in, excluding file extension. Default = "zenodo_ids". This can be provided at commandline
-            :type: str
-            :param read_in_location: path of zenodo IDs file as string. Default = "data/"
-            :type: str
+            :param zenodo_ids: list of zenodo IDs to check within zenodo API for github URL info.
+            :type: list[int]
             :param out_filename: name to include in write out filename. Saves as CSV.
             :type: str
             :param write_out_location: desired file location path as string. Default = "data/"
             :type: str
-            :param verbose: whether to print out issue data dimensions and counts. Default: True
-            :type: bool
             :returns: gh_urls_df - a pd.DataFrame of github url records from zenodo and their metadata in columns: index, ZenodoID, Title, DOI, GitHubURL, CreatedDate.
             :type: pd.DataFrame
 
@@ -87,7 +96,7 @@ class GhURLsGetter:
 
         # write-out file setup
         # get date for generating extra filename info
-        current_date_info = datetime.now().strftime(
+        current_date_info = datetime.datetime.now().strftime(
             "%Y-%m-%d"
         )  # run this at start of script not in loop to avoid midnight/long-run issues
         write_out = f"{write_out_location}{out_filename}"
@@ -97,17 +106,6 @@ class GhURLsGetter:
         total_records = len(zenodo_ids)
         num_batches = math.ceil(total_records / batch_size)  # 3
         self.logger.debug(f"Fetching urls for {total_records} records")
-
-        # rate handling setup
-        s = requests.Session()
-        retries = Retry(
-            total=10,
-            connect=5,
-            read=3,
-            backoff_factor=1,
-            status_forcelist=[202, 502, 503, 504],
-        )
-        s.mount("https://", HTTPAdapter(max_retries=retries))
 
         # 'gatherer' variables setup
         record_count = 0  # counts records with github urls
@@ -133,71 +131,71 @@ class GhURLsGetter:
                 )
 
                 # API queries loop #
-                try:
-                    # zenodo API query created
-                    records_api_url = "https://zenodo.org/api/records"
-                    record_query_url = f"{records_api_url}/{record_id}"
+                # zenodo API query created
+                records_api_url = "https://zenodo.org/api/records"
+                record_query_url = f"{records_api_url}/{record_id}"
 
-                    api_response = s.get(url=record_query_url, timeout=10)
-                    self.logger.debug(
-                        f"For record ID {record_id}, API response is {api_response}; with rate limit remaining: {api_response.headers['x-ratelimit-remaining']}"
-                    )
+                self.logger.info(f"getting json via request url {record_query_url}.")
+                api_response = run_with_retries(
+                    fn=lambda: raise_if_response_error(
+                        api_response=self.s.get(
+                            url=record_query_url,
+                            params={"access_token": self.zn_token},
+                            timeout=10,
+                        ),
+                        logger=self.logger,
+                    ),
+                    logger=self.logger,
+                )
+                assert api_response.ok, f"API response is: {api_response}"
 
-                    if "metadata" in api_response.json():
-                        # API tag info via https://developers.zenodo.org/#representation at 12 Dec 2023.
-                        record_title = api_response.json()[
-                            "title"
-                        ]  # (string) Title of deposition (automatically set from metadata).
-                        record_created = api_response.json()[
-                            "created"
-                        ]  # (timestamp) Creation time of deposition (in ISO8601 format)
-                        record_doi = api_response.json()[
-                            "doi"
-                        ]  # (string) Digital Object Identifier (DOI) ... only present for published depositions
-                        record_metadata = api_response.json()[
-                            "metadata"
-                        ]  # (object) deposition metadata resource
+                self.logger.debug(
+                    f"For record ID {record_id}, API response is {api_response}; with rate limit remaining: {api_response.headers['x-ratelimit-remaining']}"
+                )
 
-                        if "related_identifiers" in record_metadata:
-                            record_metadata_identifiers = api_response.json()[
-                                "metadata"
-                            ]["related_identifiers"]
+                if "metadata" in api_response.json():
+                    # API tag info via https://developers.zenodo.org/#representation at 12 Dec 2023.
+                    record_title = api_response.json()[
+                        "title"
+                    ]  # (string) Title of deposition (automatically set from metadata).
+                    record_created = api_response.json()[
+                        "created"
+                    ]  # (timestamp) Creation time of deposition (in ISO8601 format)
+                    record_doi = api_response.json()[
+                        "doi"
+                    ]  # (string) Digital Object Identifier (DOI) ... only present for published depositions
+                    record_metadata = api_response.json()[
+                        "metadata"
+                    ]  # (object) deposition metadata resource
 
-                            if (
-                                "github.com"
-                                in record_metadata_identifiers[0]["identifier"]
-                            ) & ("url" in record_metadata_identifiers[0]["scheme"]):
-                                # get the github url!
-                                record_gh_repo_url = record_metadata_identifiers[0][
-                                    "identifier"
-                                ]
+                    if "related_identifiers" in record_metadata:
+                        record_metadata_identifiers = api_response.json()["metadata"][
+                            "related_identifiers"
+                        ]
 
-                                # collate items into a dictionary for this record ID
-                                row_dict = {
-                                    "ZenodoID": record_id,
-                                    "Title": record_title,
-                                    "DOI": record_doi,
-                                    "GitHubURL": record_gh_repo_url,
-                                    "CreatedDate": record_created,
-                                }
-                                # print(f"{record_id}; {record_title}; {record_created}; {record_doi}; {record_gh_repo_url}")
-                                # add this completed 'row' to the chunk
-                                writeable_chunk.append(row_dict)
-                                self.logger.debug(
-                                    f"At (including) ID count {id_count} there have been {record_count} github urls records located so far."
-                                )
-                                record_count += 1
-                # handle errors
-                except TypeError as e_type:
-                    self.logger.debug(f"Type error going on: {e_type}")
-                except Exception as e:
-                    if verbose:
-                        self.logger.error(
-                            f"API response fail exception for {record_id}: {e}"
-                        )
-                    print(type(e))
-                    print(e)
-                # TODO: what do we do woith teh ereror??????????
+                        if (
+                            "github.com" in record_metadata_identifiers[0]["identifier"]
+                        ) & ("url" in record_metadata_identifiers[0]["scheme"]):
+                            # get the github url!
+                            record_gh_repo_url = record_metadata_identifiers[0][
+                                "identifier"
+                            ]
+
+                            # collate items into a dictionary for this record ID
+                            row_dict = {
+                                "ZenodoID": record_id,
+                                "Title": record_title,
+                                "DOI": record_doi,
+                                "GitHubURL": record_gh_repo_url,
+                                "CreatedDate": record_created,
+                            }
+                            # print(f"{record_id}; {record_title}; {record_created}; {record_doi}; {record_gh_repo_url}")
+                            # add this completed 'row' to the chunk
+                            writeable_chunk.append(row_dict)
+                            self.logger.debug(
+                                f"At (including) ID count {id_count} there have been {record_count} github urls records located so far."
+                            )
+                            record_count += 1
 
             # convert to pandas dataframe format
             writeable_chunk_df = pd.DataFrame.from_dict(writeable_chunk)
@@ -230,63 +228,3 @@ class GhURLsGetter:
 
         # return complete set of github url records as a dataframe
         return gh_urls_df
-
-
-# this bit
-if __name__ == "__main__":
-    """
-    get zenodo software IDs
-    write these to csv file
-    read csv file back in for comparison
-    """
-    logger = loggit.get_default_logger(
-        console=True, set_level_to="DEBUG", log_name="logs/get_gh_urls_logs.txt"
-    )
-
-    url_getter = GhURLsGetter(logger)
-
-    gh_urls_df = pd.DataFrame()
-    try:
-        # returns dataframe and saves to variable
-        gh_urls_df = url_getter.get_gh_urls(
-            config_path="zenodococode/zenodoconfig.cfg",
-            in_filename="zenodo_ids",
-            read_in_location="data/",
-            out_filename="gh_urls",
-            write_out_location="data/",
-            verbose=False,
-        )
-        if len(gh_urls_df) != 0:
-            logger.info("GitHub URL grab complete.")
-        else:
-            logger.warning(
-                "GitHub URL grab did not work, length of records returned is zero."
-            )
-    except Exception as e:
-        logger.error(
-            f"There's been an exception while trying to run get_gh_urls(): {e}"
-        )
-
-    # read in complete dataset as separate variable for comparison
-    total_urls = pd.DataFrame()
-    try:
-        current_date_info = datetime.now().strftime(
-            "%Y-%m-%d"
-        )  # run this at start of script not in loop to avoid midnight/long-run issues
-        gh_urls_file = "data/gh_urls"
-        gh_urls_file_extra_info = f"{gh_urls_file}_{current_date_info}.csv"
-        total_urls = pd.read_csv(gh_urls_file_extra_info, header=0)
-    except Exception as e:
-        logger.error(
-            f"There's been an exception while trying to read back in data generated by get_gh_urls() from {gh_urls_file_extra_info}: {e}"
-        )
-
-        # if everything is good, this shouldn't trigger.
-        try:
-            assert (
-                len(gh_urls_df.index) == len(total_urls.index)
-            ), f"WARNING! Lengths of returned df ({len(gh_urls_df)}) vs df read in from file ({len(total_urls)}) DO NOT MATCH. Did you append too many records to the gh_urls file??"
-        except AssertionError as e:
-            logger.error(
-                f"The outputs of running the function and reading back in data DO NOT MATCH; {e}"
-            )
