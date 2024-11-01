0c5e7cd264c89f125076da7d7a385365ecbef9cd
FlicAnderson <s0784624@ed.ac.uk>
2024-11-01 15:56:51 +0000

    rewrite to avoid using chunker

--- zenodocode/get_gh_urls.py
+++ zenodocode/get_gh_urls.py
@@ -58,40 +58,18 @@ class GhURLsGetter:
         write_out_location="data/",
     ):
         """
-            Read in csv of zenodo record IDs for software records and query; pull out GitHub urls from metadata;
-            save these records out into a csv file and return all records in a dataframe.
-            Logging output to file get_gh_urls_logs.txt
+        Read in csv of zenodo record IDs for software records and query; pull out GitHub urls from metadata;
+        save these records out into a csv file and return all records in a dataframe.
+        Logging output to file get_gh_urls_logs.txt
 
-            :param zenodo_ids: list of zenodo IDs to check within zenodo API for github URL info.
-            :type: list[int]
-            :param out_filename: name to include in write out filename. Saves as CSV.
-            :type: str
-            :param write_out_location: desired file location path as string. Default = "data/"
-            :type: str
-            :returns: gh_urls_df - a pd.DataFrame of github url records from zenodo and their metadata in columns: index, ZenodoID, Title, DOI, GitHubURL, CreatedDate.
-            :type: pd.DataFrame
-
-            Examples:
-            ----------
-        $ python zenodocode/get_gh_urls.py data/zenodo_ids.csv
-
-        python zenodocode/get_gh_urls.py data/zenodo_ids_x1000_2024-02-01.csv
-        Using commandline argument data/zenodo_ids_x1000_2024-02-01.csv as input file of Zenodo IDs to retrieve GH URLs for.
-        >> Running loop/chunk number 1 of 100 ...
-        >> Running loop/chunk number 2 of 100 ...
-        # ...
-        >> Running loop/chunk number 99 of 100 ...
-        >> Running loop/chunk number 100 of 100 ...
-        There are 768 records with github urls, out of 1000 records in total; saved out to data/gh_urls..
-        GitHub URL grab complete.
-
-        $ python zenodocode/get_gh_urls.py
-        # this uses default filename for input file
-        >>>> Running loop/chunk number 1 of 3:
-        >>>> Running loop/chunk number 2 of 3:
-        >>>> Running loop/chunk number 3 of 3:
-        There are 26 records with github urls, out of 30 records in total; saved out to data/gh_urls.csv.
-        GitHub URL grab complete.
+        :param zenodo_ids: list of zenodo IDs to check within zenodo API for github URL info.
+        :type: list[int]
+        :param out_filename: name to include in write out filename. Saves as CSV.
+        :type: str
+        :param write_out_location: desired file location path as string. Default = "data/"
+        :type: str
+        :returns: gh_urls_df - a pd.DataFrame of github url records from zenodo and their metadata in columns: index, ZenodoID, Title, DOI, GitHubURL, CreatedDate.
+        :type: pd.DataFrame
         """
 
         # write-out file setup
@@ -99,9 +77,9 @@ class GhURLsGetter:
         write_out = f"{write_out_location}{out_filename}"
         write_out_extra_info = f"{write_out}_{self.current_date_info}.csv"
 
-        batch_size = 10
+        # batch_size = 10
         total_records = len(zenodo_ids)
-        num_batches = math.ceil(total_records / batch_size)  # 3
+        # num_batches = math.ceil(total_records / batch_size)  # 3
         self.logger.debug(f"Fetching urls for {total_records} records")
 
         # 'gatherer' variables setup
@@ -110,118 +88,95 @@ class GhURLsGetter:
         loop_num = 0  # counts current 'chunk' loop
         gh_urls_df = pd.DataFrame()  # create empty df to hold complete record set
 
-        # this loop splits the zenodo IDs into 3x batches of size 10 and returns ephemeral
-        for i in chunker.chunker(zenodo_ids, batch_size):
+        records_list = []
+
+        records_api_url = "https://zenodo.org/api/records"
+
+        for record_id in zenodo_ids:
             loop_num += 1
             self.logger.info(
-                f"\n >> Running loop/chunk number {loop_num} of {num_batches} ... "
+                f"\n >> Running loop number {loop_num} of {total_records} ... "
             )
-            writeable_chunk = []  # reset to empty the dict of previous records
-            writeable_chunk_ids = (
-                i.to_list()
-            )  # pull IDs into a list of size 10 in this iteration of the loop
 
-            for record_id in writeable_chunk_ids:
-                id_count += 1
-                self.logger.debug(
-                    f"Running loop for record ID {record_id}, in loop {loop_num}, ID count {id_count} of {total_records}"
-                )
+            # API queries loop; zenodo API query created
+            record_query_url = f"{records_api_url}/{record_id}"
 
-                # API queries loop #
-                # zenodo API query created
-                records_api_url = "https://zenodo.org/api/records"
-                record_query_url = f"{records_api_url}/{record_id}"
-
-                self.logger.info(f"getting json via request url {record_query_url}.")
-                api_response = run_with_retries(
-                    fn=lambda: raise_if_response_error(
-                        api_response=self.s.get(
-                            url=record_query_url,
-                            params={"access_token": self.zn_token},
-                            timeout=10,
-                        ),
-                        logger=self.logger,
+            self.logger.info(f"getting json via request url {record_query_url}.")
+            api_response = run_with_retries(
+                fn=lambda: raise_if_response_error(
+                    api_response=self.s.get(
+                        url=record_query_url,
+                        params={"access_token": self.zn_token},
+                        # timeout=10,
                     ),
                     logger=self.logger,
-                )
-                assert api_response.ok, f"API response is: {api_response}"
+                ),
+                logger=self.logger,
+            )
+            assert api_response.ok, f"API response is: {api_response}"
 
-                self.logger.debug(
-                    f"For record ID {record_id}, API response is {api_response}; with rate limit remaining: {api_response.headers['x-ratelimit-remaining']}"
-                )
-
-                if "metadata" in api_response.json():
-                    # API tag info via https://developers.zenodo.org/#representation at 12 Dec 2023.
-                    record_title = api_response.json()[
-                        "title"
-                    ]  # (string) Title of deposition (automatically set from metadata).
-                    record_created = api_response.json()[
-                        "created"
-                    ]  # (timestamp) Creation time of deposition (in ISO8601 format)
-                    record_doi = api_response.json()[
-                        "doi"
-                    ]  # (string) Digital Object Identifier (DOI) ... only present for published depositions
-                    record_metadata = api_response.json()[
-                        "metadata"
-                    ]  # (object) deposition metadata resource
-
-                    if "related_identifiers" in record_metadata:
-                        record_metadata_identifiers = api_response.json()["metadata"][
-                            "related_identifiers"
-                        ]
-
-                        if (
-                            "github.com" in record_metadata_identifiers[0]["identifier"]
-                        ) & ("url" in record_metadata_identifiers[0]["scheme"]):
-                            # get the github url!
-                            record_gh_repo_url = record_metadata_identifiers[0][
-                                "identifier"
-                            ]
-
-                            # collate items into a dictionary for this record ID
-                            row_dict = {
-                                "ZenodoID": record_id,
-                                "Title": record_title,
-                                "DOI": record_doi,
-                                "GitHubURL": record_gh_repo_url,
-                                "CreatedDate": record_created,
-                            }
-                            # print(f"{record_id}; {record_title}; {record_created}; {record_doi}; {record_gh_repo_url}")
-                            # add this completed 'row' to the chunk
-                            writeable_chunk.append(row_dict)
-                            self.logger.debug(
-                                f"At (including) ID count {id_count} there have been {record_count} github urls records located so far."
-                            )
-                            record_count += 1
-
-            # convert to pandas dataframe format
-            writeable_chunk_df = pd.DataFrame.from_dict(writeable_chunk)
-
-            # write out 'completed' chunk df content to csv via APPEND (use added date filename)
-            writeable_chunk_df.to_csv(
-                write_out_extra_info,
-                mode="a",
-                index=False,
-                header=not os.path.exists(write_out_extra_info),
+            self.logger.debug(
+                f"For record ID {record_id}, API response is {api_response}; with rate limit remaining: {api_response.headers['x-ratelimit-remaining']}"
             )
 
-            # add the completed chunk of df to a 'total' df:
-            gh_urls_df = pd.concat(
-                [gh_urls_df, writeable_chunk_df],
+            if "metadata" in api_response.json():
+                # API tag info via https://developers.zenodo.org/#representation at 12 Dec 2023.
+                record_title = api_response.json()[
+                    "title"
+                ]  # (string) Title of deposition (automatically set from metadata).
+                record_created = api_response.json()[
+                    "created"
+                ]  # (timestamp) Creation time of deposition (in ISO8601 format)
+                record_doi = api_response.json()[
+                    "doi"
+                ]  # (string) Digital Object Identifier (DOI) ... only present for published depositions
+                record_metadata = api_response.json()[
+                    "metadata"
+                ]  # (object) deposition metadata resource
+
+                if "related_identifiers" in record_metadata:
+                    record_metadata_identifiers = api_response.json()["metadata"][
+                        "related_identifiers"
+                    ]
+
+                    if (
+                        "github.com" in record_metadata_identifiers[0]["identifier"]
+                    ) & ("url" in record_metadata_identifiers[0]["scheme"]):
+                        # get the github url!
+                        record_gh_repo_url = record_metadata_identifiers[0][
+                            "identifier"
+                        ]
+
+                        # collate items into a dictionary for this record ID
+                        row_dict = {
+                            "ZenodoID": record_id,
+                            "Title": record_title,
+                            "DOI": record_doi,
+                            "GitHubURL": record_gh_repo_url,
+                            "CreatedDate": record_created,
+                        }
+                        # print(f"{record_id}; {record_title}; {record_created}; {record_doi}; {record_gh_repo_url}")
+                        # add this completed 'row' to the chunk
+                        records_list.extend(row_dict)
+                        self.logger.debug(
+                            f"At (including) ID count {id_count} there have been {record_count} github urls records located so far."
+                        )
+                        record_count += 1
+
+            # create a df from records_list:
+            gh_urls_df = pd.DataFrame(records_list)
+
+            # write out 'completed' chunk df content to csv via APPEND (use added date filename)
+            gh_urls_df.to_csv(
+                write_out_extra_info,
+                mode="w",
+                index=False,
+                header=not os.path.exists(write_out_extra_info),
             )
 
         # print(f"There are {record_count} records with github urls, out of {total_records} records in total; saved out to {write_out_extra_info}.")
         self.logger.info(
             f"\n ... ENDING RUN. There are {record_count} records with github urls, out of {total_records} records in total; saved out to {write_out_extra_info}."
         )
-
-        # report on status
-        if record_count >= (total_records / 2):
-            self.logger.debug("More than half of records have GitHub urls. Nice. ")
-        else:
-            self.logger.debug(
-                "Less than half of records have GitHub urls... Something could be wrong?"
-            )
-
         # return complete set of github url records as a dataframe
         return gh_urls_df
