af8e2cee5c875b5f2efe3ff041811f0c9d07ca9b
FlicAnderson <s0784624@ed.ac.uk>
2025-06-04 16:22:23 +0100

    this was very bad and now it is slightly better

--- githubanalysis/analysis/calc_days_since_repo_creation.py
+++ githubanalysis/analysis/calc_days_since_repo_creation.py
@@ -2,34 +2,30 @@
 
 import pandas as pd
 import datetime
-
-
-import githubanalysis.processing.TO_REFACTOR__get_repo_creation_date as createdate
+import logging
+import githubanalysis.processing.get_repo_creation_date as createdate
 
 
 def calc_days_since_repo_creation(
     date,
     repo_name,
-    since_date=None,
+    config_path: str,
+    in_notebook: bool,
+    logger: logging.Logger | None,
+    since_date: datetime.datetime | None = None,
     return_in="whole_days",
-    config_path="githubanalysis/config.cfg",
 ):
     """
     Takes date and calculates number of days since that date from the date a given repo was created.
     For faster performance, since_date (ie date repo was created) can be supplied if already known.
 
     :param date: date to calculate number of days to, since creation date of repo.
-    :type: datetime
     :param repo_name: cleaned `repo_name` string without GitHub url root or trailing slashes.
-    :type: str
     :param since_date: date of creation date of repo (optional). If since_date is None, function will use get_repo_creation_date() to obtain it.
-    :type: datetime
     :param return_in: format to return days since repo creation in (e.g. just number of days vs with hours etc. Default: 'whole_days'.)
-    :type: str
     :param config_path: file path of config.cfg file containing GitHub Access Token. Default='githubanalysis/config.cfg'.
-    :type: str
     :return days_since: days_since input `date` since repo creation date for repo `repo_name`.
-    :type: int (return_in='days') / pd.Timedelta (return_in not 'days')
+    :rtype: int (return_in='days') / pd.Timedelta (return_in not 'days')
     """
 
     # check date inputs are correct format
@@ -51,9 +47,14 @@ def calc_days_since_repo_creation(
         )
 
     if since_date is None:  # get repo creation date from GH connection .created_at info
-        since_date = createdate.get_repo_creation_date(
-            repo_name, config_path=config_path, verbose=False
+        repo_dater = createdate.RepoDater(
+            repo_name=repo_name,
+            config_path=config_path,
+            in_notebook=in_notebook,
+            logger=logger,
         )
+
+        since_date = repo_dater.get_repo_creation_date()
 
     # important bit:
     days_since = date - since_date
