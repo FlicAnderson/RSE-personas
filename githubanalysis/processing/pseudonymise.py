8825a39631af255cdda85db3f6cb91eb1693a63f
FlicAnderson <s0784624@ed.ac.uk>
2025-09-03 14:14:22 +0100

    script for pseudonymising dataset for publication

---
+++ githubanalysis/processing/pseudonymise.py
@@ -0,0 +1,12 @@
+"""Pseudonymisation of research data outputs for publication"""
+
+# Files requiring this for ECEASST publication:
+# orig_data_file = "../../data/merged-data-per-dev_x2868-repos_2025-05-10.csv"
+# analysis_dataset_file = "../../data/analysis_run_sample_45pc_2025-05-12/clustered_sample_data_with_labels__2025-05-12.csv"
+# # via:  "../../data/merged-data-per-dev_x2868-repos_2025-05-10.csv" and merged-interactions-data-per-dev_x2946-repos_2025-05-12.csv
+# repo_stats_file = "../../data/summarised_SAMPLE_repos_stats_2025-05-09.csv"
+# study_repos_file = "../../data/study-sample-repo-names_2025-05-01_x2981.txt"
+
+
+# functions/scripts requiring this:
+# MANY MANY SCRIPTS, see issue ticket RSE-personas/issues/2:  #2
