397a559b7b3e528c66fec3d42e79ebe620e4679c
FlicAnderson <s0784624@ed.ac.uk>
2025-09-22 14:54:48 +0100

    utility: bash script to split repo-names list into equal batches for running on

---
+++ repo_names_splitter.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Configuration stuff
+
+fspec=qq.c
+num_files=14
+
+# Work out lines per file.
+
+total_lines=$(wc -l <${fspec})
+((lines_per_file = (total_lines + num_files - 1) / num_files))
+
+# Split the actual file, maintaining lines.
+
+split --lines=${lines_per_file} ${fspec} data/sample_repo_names_list_2025-03-31_x2632.txt
+
+# Debug information
+
+echo "Total lines     = ${total_lines}"
+echo "Lines  per file = ${lines_per_file}"    
+wc -l  data/sample_repo_names_list_2025-03-31_x2632.*
