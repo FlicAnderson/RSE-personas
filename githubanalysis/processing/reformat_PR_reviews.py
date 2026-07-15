7d81550f5302e3c33185ff2bff08412f1dc466f8
FlicAnderson <s0784624@ed.ac.uk>
2026-07-15 19:03:33 +0100

    fix: printing dtypes

--- githubanalysis/processing/reformat_PR_reviews.py
+++ githubanalysis/processing/reformat_PR_reviews.py
@@ -1,5 +1,6 @@
 """Reformat raw PR reviews json data from files into pd.DataFrames"""
 
+import csv
 import pandas as pd
 import re
 from pathlib import Path
@@ -117,7 +118,9 @@ class ReviewsFormatter(LocationSetup):
                 PR_reviews_file,
                 dtype=object,  # type:ignore
             )  # load in JSON from the file
-            print("json:", rough_df.dtypes)  # temporary!
+            print(
+                "input", PR_reviews_file, "json:", list(rough_df.dtypes)
+            )  # temporary!
         else:  # it'll be discussions, so...
             rough_df = pd.read_csv(
                 PR_reviews_file,
@@ -125,7 +128,7 @@ class ReviewsFormatter(LocationSetup):
                 low_memory=False,  # load from CSV file
                 dtype=object,
             )
-            print("csv:", rough_df.dtypes)  # temporary!
+            print("input", PR_reviews_file, "csv:", list(rough_df.dtypes))  # temporary!
         if rough_df.empty:
             raise RuntimeError(f"File '{PR_reviews_file}' is empty of content")
 
@@ -288,7 +291,7 @@ class ReviewsFormatter(LocationSetup):
                 f"Unknown reviews_type: {reviews_type}"
             )  # handle this error! oughtn't occur due to the assert at the start tho
         self.reformatted_PR_reviews = rough_df  # save processed df to reformatted_PR_reviews in class for reuse elsewhere
-        print(rough_df.dtypes)  # temporary!
+        print("OUTPUT FILE", list(rough_df.dtypes))  # temporary!
         return self.reformatted_PR_reviews
 
     # def reformat_PR_reviews_from_file(self, PR_reviews_file: str):
@@ -316,7 +319,12 @@ class ReviewsFormatter(LocationSetup):
 
         if self.reformatted_PR_reviews is not None:
             self.reformatted_PR_reviews.to_csv(
-                path_or_buf=write_out, mode="w", index=False, header=True
+                path_or_buf=write_out,
+                index=False,
+                header=True,
+                na_rep="",
+                mode="w",
+                quoting=csv.QUOTE_ALL,  # for safety of data: forces everything to string shapes....
             )
             self.logger.info(
                 f"wrote out dataframe of {reviews_type} PR reviews to file {write_out} for repo {self.repo_name}"
