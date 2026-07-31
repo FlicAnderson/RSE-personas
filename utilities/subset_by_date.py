7997ce034743bf6161b0e7e56b7c7417afdb24e7
FlicAnderson <s0784624@ed.ac.uk>
2026-07-31 14:03:02 +0100

    fix: try pd.timestamp instead of datetime to avoid fails

--- utilities/subset_by_date.py
+++ utilities/subset_by_date.py
@@ -1,14 +1,15 @@
 """This (set of) function(s) are to help subset the dataset via dates for valid RSE Persona calculations."""
 
 import pandas as pd
-import datetime
 
 
 def subset_by_dates(
     df: pd.DataFrame,
     datestamp_column=str,
-    from_datestamp: datetime.datetime = datetime.datetime.min,  # default to earliest possible year - not sensible, but doesn't change behaviour :C
-    to_datestamp: datetime.datetime = datetime.datetime.today(),  # defaults to today, which is the latest possible date, no behaviour changed.
+    from_datestamp=pd.Timestamp.min,  # default to earliest possible year - not sensible, but doesn't change behaviour :C
+    to_datestamp=pd.Timestamp(
+        "today"
+    ),  # defaults to today, which is the latest possible date, no behaviour changed.
 ):
     """
     There should be a function that subsets the reviews'
