830800cb16bb8edb73ab13cf8dc37d25772f6471
FlicAnderson <s0784624@ed.ac.uk>
2026-07-31 16:03:00 +0100

    fix: rework date subset function to improve logs (via print)

--- utilities/subset_by_date.py
+++ utilities/subset_by_date.py
@@ -5,19 +5,46 @@ import pandas as pd
 
 def subset_by_dates(
     df: pd.DataFrame,
-    datestamp_column=str,
-    from_datestamp=pd.Timestamp.min,  # default to earliest possible year - not sensible, but doesn't change behaviour :C
+    datestamp_column=pd.Timestamp,
+    from_datestamp=pd.Timestamp.min.date(),  # default to earliest possible year - not sensible, but doesn't change behaviour :C
     to_datestamp=pd.Timestamp(
         "today"
-    ),  # defaults to today, which is the latest possible date, no behaviour changed.
+    ).date(),  # defaults to today, which is the latest possible date, no behaviour changed. Set to "2025-04-23" for latest GH data collection date from FIRST collection (commits).
 ):
     """
-    There should be a function that subsets the reviews'
-    interactions to between specific date/timestamps in a specific column
-    to assist with future calculations and analysis.
+    There should be a function that subsets repository interactions data
+    dfs to between specific date/timestamps in a specific column, to
+    assist with future calculations and analysis.
+
+    !!!!!!!!!!!!
+    Latest data collection date for the FIRST data type collected
+    from the GH API was: "2025-04-23" for commits data.
+    !!!!!!!!!!!!
 
     Assumes UTC timezone as this is the default GH timezone.
+
+    Note: if you're having comparison issues, try the following on the df
+    and original datestamp_column to create a new one in the right format:
+
+    df["review_date_only"] = pd.to_datetime(df.original_datestamp_column)
+    df["review_date_only"] = df["review_date_only"].apply(lambda x: pd.Timestamp.date(x))
+
     """
-    return df[
-        (df[datestamp_column] > from_datestamp) & (df[datestamp_column] < to_datestamp)
+    orig_len = len(df)
+    print(f"orig_len = {len(df)}")
+
+    output = df[
+        (df[datestamp_column] > from_datestamp)
+        & (
+            df[datestamp_column] < to_datestamp
+        )  # df retains only rows AFTER from_datestamp AND BEFORE to_datestamp!
     ]
+
+    print(f"post_subset_len = {len(output)}")
+    output_len = len(output)
+
+    print(
+        f"Dropping {orig_len - output_len} rows ({(((orig_len - output_len) / orig_len) * 100):.3f}%) from df as later than date {to_datestamp}"
+    )
+
+    return output
