58935cbf8906b42f965e39c51e89b096560124e6
FlicAnderson <s0784624@ed.ac.uk>
2026-09-21 15:06:04 +0100

    feat: add logger usage

--- utilities/subset_by_date.py
+++ utilities/subset_by_date.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import datetime
+from logging import Logger
+import utilities.get_default_logger as loggit
 
 
 def subset_by_dates(
@@ -12,7 +14,8 @@ def subset_by_dates(
     to_datestamp: datetime.date | str = pd.Timestamp(
         "today"
     ).date(),  # defaults to today, which is the latest possible date, no behaviour changed. Set to "2024-11-21" for earliest GH data collection date from FIRST collection (commits).
-):
+    logger: None | Logger = None,
+) -> pd.DataFrame:
     """
     There should be a function that subsets repository interactions data
     dfs to between specific date/timestamps in a specific column, to
@@ -20,7 +23,7 @@ def subset_by_dates(
 
     !!!!!!!!!!!!
     Latest data collection date for the FIRST data type collected
-    from the GH API was: "2025-04-23" for commits data.
+    from the GH API was: "2024-11-21" for commits data.
     !!!!!!!!!!!!
 
     Assumes UTC timezone as this is the default GH timezone.
@@ -32,19 +35,35 @@ def subset_by_dates(
     df["review_date_only"] = df["review_date_only"].apply(lambda x: pd.Timestamp.date(x))
 
     """
-    orig_len = len(df)
-    print(f"orig_len = {len(df)}")
+    if logger is not None:
+        logger = logger
+    else:
+        logger = loggit.get_default_logger(
+            console=True,
+            set_level_to="DEBUG",
+            log_name="logs/subset_by_dates.txt",
+            in_notebook=False,
+        )
 
-    print(f"data type of datestamp_column is {df[datestamp_column].dtype}")
+    logger.info(f"datestamp column used for date filtering is: {datestamp_column}.")
+    logger.info(f"from_datestamp used for START date filtering is: {from_datestamp}.")
+    logger.info(f"to_datestamp used for END date filtering is: {to_datestamp}.")
+
+    orig_len = len(df)
+    logger.info(f"orig_len = {len(df)}")
+
+    logger.info(f"data type of datestamp_column is {df[datestamp_column].dtype}")
 
     df["datestamp_column_temp"] = pd.to_datetime(  # make new column
         df[datestamp_column]
     )  # change type from string to datetime
 
-    print(
+    logger.info(
         f"type of df['datestamp_column_temp'] is {type(df['datestamp_column_temp'])} ; dtypes of df are: {df.dtypes}"
     )
-    print(f"df['datestamp_column_temp'][0:5] is: {df['datestamp_column_temp'][0:5]}")
+    logger.info(
+        f"df['datestamp_column_temp'][0:5] is: {df['datestamp_column_temp'][0:5]}"
+    )
 
     df["datestamp_column_temp"] = df["datestamp_column_temp"].apply(
         lambda x: pd.Timestamp.date(
@@ -71,10 +90,10 @@ def subset_by_dates(
     # drop extra column created for subsetting with
     output = output.drop(columns=["datestamp_column_temp"], errors="raise")
 
-    print(f"post_subset_len = {len(output)}")
+    logger.info(f"post_subset_len = {len(output)}")
     output_len = len(output)
 
-    print(
+    logger.info(
         f"Dropping {orig_len - output_len} rows ({(((orig_len - output_len) / orig_len) * 100):.3f}%) from df as later than date {to_datestamp}"
     )
 
